c5e1fa3a466d4d81e4de53e2e31df0a6045e46b8
waiscodes <b.wais@outlook.com>
2021-06-21 16:49:54 -0600

    created a hello world method to test

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,3 +1,9 @@
 // the license removes the red underline
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+
+contract HelloWorld {
+  function hello() public pure returns (string memory){
+    return "hello world";
+  }
+}
