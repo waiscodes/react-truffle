9cb896abff450387fcbe90442fe796ff890293a1
waiscodes <b.wais@outlook.com>
2021-06-21 17:38:18 -0600

    added a contructor and set message method to my contract

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,7 +3,18 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract HelloWorld {
-  function hello() public pure returns (string memory){
-    return "hello world";
+
+  string public message;
+
+  constructor(string memory _message) public {
+    message = _message;
+  }
+
+  function hello() public view returns (string memory){
+    return message;
+  }
+
+  function setMessage(string memory _message) public {
+    message = _message;
   }
 }
