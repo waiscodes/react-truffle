0ff2c0fe7c69656b906e84c681ec659777090e2f
waiscodes <b.wais@outlook.com>
2021-06-21 17:40:01 -0600

    added changes to migration file

--- migrations/2_hello_world_migration.js.js
+++ migrations/2_hello_world_migration.js.js
@@ -1,5 +1,6 @@
 const HelloWorld = artifacts.require("HelloWorld");
 
 module.exports = function (deployer) {
-  deployer.deploy(HelloWorld);
+  // Arguments to methods are added using the comma. If there are more arguments just add more commas.
+  deployer.deploy(HelloWorld, "Hello world from the constructor");
 };
