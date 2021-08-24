3e5f5472a794f0d58a571f287cfdf21c86850c0a
Birm Wais <b.wais@outlook.com>
2021-08-23 18:42:53 -0600

    created a require message

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,21 +3,20 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract HelloWorld {
+    string public message;
+    address owner;
 
-  string public message;
-  address owner;
+    constructor(string memory _message) public {
+        message = _message;
+        owner = msg.sender;
+    }
 
-  constructor(string memory _message) public {
-    message = _message;
-    owner = msg.sender;
-  }
+    function hello() public view returns (string memory) {
+        return message;
+    }
 
-  function hello() public view returns (string memory){
-    return message;
-  }
-
-  function setMessage(string memory _message) public {
-    require(msg.sender == owner, "You're not the owner my guy");
-    message = _message;
-  }
+    function setMessage(string memory _message) public {
+        require(msg.sender == owner, "You're not that guy pal");
+        message = _message;
+    }
 }
