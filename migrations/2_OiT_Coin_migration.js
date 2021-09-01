dd62b9769753f73feee00aea0502f94add943c93
Birm Wais <b.wais@outlook.com>
2021-08-31 22:45:53 -0600

    replaced the hello world migration with oit coin

--- migrations/2_OiT_Coin_migration.js
+++ migrations/2_OiT_Coin_migration.js
@@ -1,7 +1,7 @@
-const HelloWorld = artifacts.require("HelloWorld");
+const OiTCoin = artifacts.require("OiTCoin");
 
 // To Redeploy this contract on local chain but to a new address:> migrate --reset
 module.exports = function (deployer) {
   // Arguments to methods are added using the comma. If there are more arguments just add more commas.
-  deployer.deploy(HelloWorld, "Hello world from the constructor");
+  deployer.deploy(OiTCoin, "Hello world from the constructor");
 };
